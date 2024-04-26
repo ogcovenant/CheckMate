7d2c098780a280295c2028aeccf2120d859b60a2
Covenant Ogowale <justcovenant@outlook.com>
2024-04-26 02:42:18 +0100

    fixed the deployment bug by adding the modal functinality to the tomorrow and this-week page

--- src/pages/ThisWeek.tsx
+++ src/pages/ThisWeek.tsx
@@ -1,9 +1,25 @@
-import { Box, Heading, Text, Button } from "@chakra-ui/react"
+import {
+  Box,
+  Heading,
+  Text,
+  Button,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure
+} from "@chakra-ui/react";
 import TodoContent from "../components/TodoContent"
 import { FaPlus } from "react-icons/fa6"
 
 
 const ThisWeek = () => {
+
+  const { isOpen, onClose, onOpen } = useDisclosure()
+
   return (
     <>
       <Box p={5} minWidth={"50%"} maxWidth={"90%"} height={"95vh"} overflowY={"auto"}>
@@ -18,8 +34,22 @@ const ThisWeek = () => {
             <Button backgroundColor={"#ffcc24"} display={"flex"} alignItems={"center"} gap={2} _hover={{ backgroundColor: "#ffcc24" }}><FaPlus />Add Task</Button>
           </Box>
         </Heading>
-        <TodoContent />
+        <TodoContent onOpen={onOpen} />
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>
+            <Text>Event</Text>
+            <ModalCloseButton />
+          </ModalHeader>
+          <ModalBody>
+            <Text>Testing</Text>
+          </ModalBody>
+          <ModalFooter></ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   )
 }
