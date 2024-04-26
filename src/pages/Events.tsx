cdd3f0a4131853e68bc1934de8f72011ffd654ec
Covenant Ogowale <justcovenant@outlook.com>
2024-04-27 00:04:04 +0100

    added add event modal

--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,16 +1,19 @@
-import { Box, Text, Heading, Button, Input, InputGroup, InputLeftElement, VStack } from "@chakra-ui/react"
+import { Box, Text, Heading, Button, Input, InputGroup, InputLeftElement, VStack, useDisclosure } from "@chakra-ui/react"
 import { FaPlus } from "react-icons/fa6"
 import { FaSearch } from "react-icons/fa"
 import EventItem from "../components/EventItem"
+import AddEventModal from "../components/AddEventModal"
 
 const Events = () => {
+
+  const { isOpen: isAddOpen, onClose: onAddClose, onOpen: onAddOpen } = useDisclosure()
 
   return (
     <>
       <Box p={5} width={"90%"} height={"96vh"} overflow={"auto"}>
         <Heading display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
           <Text fontSize={"3xl"}>Events</Text>
-          <Button display={"flex"} gap={2} alignItems={"center"} backgroundColor={"#ffcc24"} _hover={{ backgroundColor:"#ffcc24" }}><FaPlus />Add Event</Button>
+          <Button display={"flex"} gap={2} alignItems={"center"} backgroundColor={"#ffcc24"} _hover={{ backgroundColor:"#ffcc24" }} onClick={onAddOpen}><FaPlus />Add Event</Button>
         </Heading>
         <Box>
           <Box mt={5}>
@@ -35,6 +38,8 @@ const Events = () => {
           </VStack>
         </Box>
       </Box>
+
+      <AddEventModal isOpen={isAddOpen} onClose={onAddClose}/>
     </>
   )
 }
