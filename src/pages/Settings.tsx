38798d147ab3928202a906e9a32d50610aed5857
Covenant Ogowale <justcovenant@outlook.com>
2024-04-28 18:13:17 +0100

    added settings base view

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,8 +1,36 @@
+import { Box, Text, Heading, VStack } from "@chakra-ui/react"
+import { ArrowRight2 } from "iconsax-react"
 
 const Settings = () => {
   return (
     <>
-      
+      <Box p={3} width={"90%"}>
+        <Heading>
+          <Text fontSize={"3xl"} fontWeight={700}>Settings</Text>
+        </Heading>
+        <Box mt={5}>
+          <VStack gap={5}>
+            <Box w={"full"} p={1} pb={3} borderBottom={"1px solid #0000003f"} display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
+              <Box>
+                <Text fontSize={"lg"} fontWeight={600}>Account Settings</Text>
+              </Box>
+              <ArrowRight2 size="32" color="#000"/>
+            </Box>
+            <Box w={"full"} p={1} pb={3} borderBottom={"1px solid #0000003f"} display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
+              <Box>
+                <Text fontSize={"lg"} fontWeight={600}>Notification Settings</Text>
+              </Box>
+              <ArrowRight2 size="32" color="#000"/>
+            </Box>
+            <Box w={"full"} p={1} pb={3} borderBottom={"1px solid #0000003f"} display={"flex"} justifyContent={"space-between"} alignItems={"center"}>
+              <Box>
+                <Text fontSize={"lg"} fontWeight={600}>Export Data</Text>
+              </Box>
+              <ArrowRight2 size="32" color="#000"/>
+            </Box>
+          </VStack>
+        </Box>
+      </Box>
     </>
   )
 }
