c5ff6bae6ef29a6df553faa1e644e25dfee6db5c
Covenant Ogowale <justcovenant@outlook.com>
2024-04-29 02:05:48 +0100

    finished login page responsiveness

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -5,20 +5,20 @@ import onboarding from "../assets/onboarding.png";
 const Login = () => {
   return (
     <>
-      <Box height={"100vh"} display={"flex"} justifyContent={"space-between"}>
+      <Box height={"100vh"} display={"flex"} flexDirection={{ base:"column", lg:"row" }} alignItems={{ lg:"center" }} justifyContent={{ base:"center", xl:"space-between" }}>
         <Box
           background={"black"}
-          width={"49%"}
-          height={"100%"}
+          width={{ base:"100%", lg:"40vh", xl:"49%" }}
+          height={{ base:"40vh", md:"100vh", xl:"100%" }}
           display={"flex"}
           justifyContent={"center"}
           alignItems={"center"}
         >
-          <Image src={onboarding} width={400} />
+          <Image src={onboarding} width={{ base:150, md:200, lg:300, xl:400 }} />
         </Box>
         <Box
-          width={"50%"}
-          height={"100%"}
+          width={{ base:"100%", lg:"60vh", xl:"50%" }}
+          height={{ base:"60vh", lg:"100%" }}
           display={"flex"}
           justifyContent={"center"}
           p={50}
@@ -106,7 +106,7 @@ const Login = () => {
                 </Button>
               </Box>
             </Box>
-            <Text mt={3} textAlign={"center"} fontWeight={500}>
+            <Text mt={3} textAlign={"center"} fontWeight={500} fontSize={14} mb={{ base:5, lg:0 }}>
               Don't have an account?{" "}
               <Link as={link} to="/signup">
                 {" "}
